cba1f4aeff0b2675599235e9c5a9e438a2575ed7
Meisuleen Kased <meisuleen@gmail.com>
2018-12-14 05:47:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,27 @@
 class Song
-  attr_accessor :name, :artist, :songs
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
 
+  # you need to pull an artist name out of the filename first
+  # Then you can call artist_name= on the new song
+
+
   def self.new_by_filename(file_name)
     song_data = file_name.split(" - ")
     song_name = song_data[1]
     new_song = self.new(song_name)
-    artist = new_song.artist
+    artist = new_song.artist #<= you need to pull an artist name out of the filename first
     new_song
   end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
 
 
 end
